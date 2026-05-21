33abaa29775b09611337a69ebe8e2214baa7c888
Dannys02 <webdannys@gmail.com>
2026-05-21 22:59:53 +0700

    menambahkan section filosofi dalam file Depan.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,18 @@
-import Depan from './assets/pages/Depan'
-
-function App() {
-  return (
-    <>
-      <Depan />
-    </>
-  )
-}
+import Depan from "./assets/pages/Depan";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Depan />} />
+
         {/* Hidden route for login */}
         <Route path="/auth/v1/secure-login" element={<Login />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
